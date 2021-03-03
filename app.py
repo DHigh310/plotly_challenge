0ce44de22103a71a285b6cf4779bb83b797af2ba
David K High Jr <dhigh@Davids-MBP.hsd1.il.comcast.net>
2021-03-02 23:23:08 -0600

    Getting there

--- app.py
+++ app.py
@@ -10,4 +10,14 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+print(os.environ.get('DATABASE_URL', ''))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/bellybutton.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
 
+
+# reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(db.engine, reflect=True)
